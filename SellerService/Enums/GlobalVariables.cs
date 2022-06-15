d909082a196d6632fec4ba4d0c62dc6939dfcf3d
Maninder Gill <manindersingh.gill@cognizant.com>
2022-06-15 16:51:11 -0400

    Adding Model, enum.

---
+++ SellerService/Enums/GlobalVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellerService.Enums
+{
+    public class GlobalVariables
+    {
+        public enum ProductCategory
+        { 
+            Painting,
+            Sculptor,
+            Ornament
+        }
+    }
+}
